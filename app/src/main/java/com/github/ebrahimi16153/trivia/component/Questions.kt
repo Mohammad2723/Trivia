48b2a9be49fb247d80df665cc445ba70277f61dd
mohammad2723 <kotlin2723@gmail.com>
2022-11-23 16:21:18 +0330

    QuestionDisplay is implement but next question and preQuestion is not implement

--- app/src/main/java/com/github/ebrahimi16153/trivia/component/Questions.kt
+++ app/src/main/java/com/github/ebrahimi16153/trivia/component/Questions.kt
@@ -1,11 +1,34 @@
 package com.github.ebrahimi16153.trivia.component
 
 import android.util.Log
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import com.github.ebrahimi16153.trivia.model.Question
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.github.ebrahimi16153.trivia.model.QuestionItem
+import com.github.ebrahimi16153.trivia.util.myColor
 import com.github.ebrahimi16153.trivia.viewmodel.QuestionsViewModel
 
 
@@ -15,14 +38,176 @@ fun Questions(viewModel: QuestionsViewModel) {
 
     if (viewModel.data.value.isLoading == true) {
         CircularProgressIndicator(color = Color(0xFF007AFF))
-        Log.d("Loading...", "LOADING >>>>")
+
 
     } else {
         Log.d("loading...", "LOADING Successfully >>>>")
-        question?.forEach {
-            Log.d("Result", it.question.toString())
-        }
+//        question?.forEach {
+//            Log.d("Result", it.question.toString())
+//            
+//        }
+        question?.first()?.let { QuestionDisplay(question = it) }
 
     }
 }
 
+
+//@Preview
+@Composable
+fun QuestionDisplay(
+    question: QuestionItem,
+//    questionIndex: MutableState<Int>,
+//    viewModel: QuestionsViewModel,
+    onNextClick: (Int) -> Unit = {}
+) {
+
+    val choiceState = remember {
+        question.choices?.toMutableList()
+    }
+    val answerState = remember {
+        mutableStateOf<Int?>(null)
+    }
+    val correctAnswerState = remember {
+        mutableStateOf<Boolean?>(null)
+    }
+    val updateAnswer: (Int) -> Unit = remember(question) {
+        {
+            answerState.value = it
+            correctAnswerState.value = choiceState?.get(it) == question.answer
+        }
+    }
+
+    Surface(
+        modifier = Modifier
+            .fillMaxSize(), color = myColor().background
+    ) {
+        Column(
+            modifier = Modifier.padding(all = 20.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
+            QuestionTracker()
+//            DottedLine( )
+            Line()
+
+            Column(modifier = Modifier.padding(top = 20.dp)) {
+                question.question?.let {
+                    Text(
+                        text = it,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight(0.3f)
+                            .align(alignment = Alignment.Start),
+                        fontSize = 17.sp,
+                        fontWeight = FontWeight.Bold,
+                        lineHeight = 22.sp,
+                        color = myColor().text
+                    )
+                }
+            }
+
+            // list of radio button -> choices
+            choiceState?.forEachIndexed { index, answerTex ->
+
+                Row(
+                    modifier = Modifier
+                        .padding(3.dp)
+                        .fillMaxWidth()
+                        .height(45.dp)
+                        .border(
+                            width = 4.dp,
+                            brush = Brush.linearGradient(
+                                colors = listOf(
+                                    myColor().primary,
+                                    myColor().fail
+                                )
+                            ), shape = RoundedCornerShape(15.dp)
+                        )
+                        .clip(
+                            RoundedCornerShape(
+                                topStartPercent = 50,
+                                topEndPercent = 50,
+                                bottomEndPercent = 50,
+                                bottomStartPercent = 50
+                            )
+                        )
+                        .background(Color.Transparent),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(
+                        selected = (answerState.value == index),
+                        onClick = { updateAnswer(index) },
+                        modifier = Modifier.padding(start = 16.dp),
+                        colors = RadioButtonDefaults
+                            .colors(
+                                selectedColor =
+                                if (correctAnswerState.value == true && index == answerState.value) {
+                                    myColor().successes
+                                } else {
+                                    myColor().fail
+                                }))
+                    Text(text = answerTex!!)
+
+                }
+            }
+        }
+    }
+}
+
+//@Preview
+@Composable
+fun QuestionTracker(counter: Int = 10, outOff: Int = 100) {
+
+    Text(text = buildAnnotatedString {
+        withStyle(style = ParagraphStyle(textIndent = TextIndent.None)) {
+            withStyle(
+                style = SpanStyle(
+                    color = myColor().text,
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 30.sp
+                )
+            ) {
+
+                append("Question $counter/")
+                withStyle(
+                    SpanStyle(
+                        color = myColor().text,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 20.sp
+                    )
+                ) {
+                    append("$outOff")
+                }
+            }
+        }
+    }, modifier = Modifier.padding(20.dp))
+
+}
+
+@Composable
+fun DottedLine(pathEffect: PathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)) {
+    val color: Color = myColor().text
+    Canvas(modifier = Modifier
+        .fillMaxWidth()
+        .height(1.dp), onDraw = {
+        drawLine(
+            color = color,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, y = 0f),
+            pathEffect = pathEffect
+        )
+    })
+
+}
+
+@Composable
+fun Line() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(0.5.dp)
+            .alpha(0.7f), color = myColor().text
+    ) {
+
+    }
+}
--- app/src/main/java/com/github/ebrahimi16153/trivia/screens/TriviaHome.kt
+++ app/src/main/java/com/github/ebrahimi16153/trivia/screens/TriviaHome.kt
@@ -1,83 +1,42 @@
 package com.github.ebrahimi16153.trivia.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.RadioButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.github.ebrahimi16153.trivia.component.QuestionDisplay
 import com.github.ebrahimi16153.trivia.component.Questions
+import com.github.ebrahimi16153.trivia.model.QuestionItem
 import com.github.ebrahimi16153.trivia.util.myColor
 import com.github.ebrahimi16153.trivia.viewmodel.QuestionsViewModel
 
 
 @Composable
-fun TriviaHome(viewModel: QuestionsViewModel = hiltViewModel()) {
+fun TriviaHome(viewModel: QuestionsViewModel = hiltViewModel()) = Questions(viewModel = viewModel)
 
-    Questions(viewModel = viewModel)
-    TriviaDisplay()
 
-}
 
-@Preview
-@Composable
-fun TriviaDisplay() {
-    Surface(
-        modifier = Modifier
-            .fillMaxSize(), color = myColor().background
-    ) {
-        Column(
-            modifier = Modifier.padding(top = 20.dp),
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.Start
-        ) {
-            QuestionTracker()
-
-        }
-    }
-}
-
-//@Preview
-@Composable
-fun QuestionTracker(counter: Int = 10, outOff: Int = 100) {
-
-    Text(text = buildAnnotatedString {
-        withStyle(style = ParagraphStyle(textIndent = TextIndent.None)) {
-            withStyle(
-                style = SpanStyle(
-                    color = myColor().text,
-                    fontWeight = FontWeight.ExtraBold,
-                    fontSize = 30.sp
-                )
-            ) {
-
-                append("Question $counter/")
-                withStyle(
-                    SpanStyle(
-                        color = myColor().text,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 20.sp
-                    )
-                ) {
-                    append("$outOff")
-                }
-
-            }
-        }
-
-    }, modifier = Modifier.padding(20.dp))
-    
-}
